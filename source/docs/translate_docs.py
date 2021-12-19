716ffbe50aa5d92654bf723e50a961b833fe84bb
Andreas Siebel <a.siebel@posteo.de>
2021-12-19 17:09:13 +0100

    code refactoring in translate

--- source/docs/translate_docs.py
+++ source/docs/translate_docs.py
@@ -2,22 +2,15 @@ import deepl
 import os
 from pathlib import Path
 import deepl_key
-print(deepl_key.KEY)
 translator = deepl.Translator(deepl_key.KEY)
-
 path = os.getcwd()
-print(path)
 
 for root, dirs, files in os.walk("./source/objectsfirst_german/"):
     for name in files:
         file = os.path.join(root, name)
-        print(file)
-        print(os.path.join(root, name))
         target_file = os.path.join(root, name).replace("german", "english")
-        
         txt_de = Path(file).read_text()
         txt_en = translator.translate_text(txt_de, target_lang="EN-US")
-        print(txt_de)
         output = open(target_file, 'w')
         output.write(str(txt_en))
         output.close()
