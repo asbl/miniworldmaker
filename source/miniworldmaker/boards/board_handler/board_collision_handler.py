fcb0a55aedeea7b9c82306e6877e50bb0f6477a9
Andreas Siebel <a.siebel@posteo.de>
2021-12-20 08:02:05 +0100

    added on_not_sensing methods for kara

--- source/miniworldmaker/boards/board_handler/board_collision_handler.py
+++ source/miniworldmaker/boards/board_handler/board_collision_handler.py
@@ -6,25 +6,43 @@ class BoardCollisionHandler:
         self.board = board
 
     def handle_all_collisions(self):
-        self._handle_collision_with_tokens()
-        self._handle_collision_with_borders()
-        self._handle_on_board()
-        self._handle_not_on_board()
+        self._handle_token_sensing_token_methods()
+        self._handle_token_not_sensing_token_methods()
+        self._handle_token_sensing_border_methods()
+        self._handle_token_sensing_on_board_methods()
+        self._handle_token_sensing_not_on_board_methods()
 
-    def _handle_collision_with_tokens(self):
+    def _handle_token_sensing_token_methods(self):
         for method in self.board.event_handler.registered_events["on_sensing_token"].copy():
             token = method.__self__
             token_type_of_target = method.__name__[11:]
             found_tokens_for_token_type = token.sensing_tokens(token_filter=token_type_of_target)
+            if found_tokens_for_token_type == None: # found nothing
+                found_tokens_for_token_type = []
+            if token in found_tokens_for_token_type: # found self
+                found_tokens_for_token_type.remove(token)
+            for found_token in found_tokens_for_token_type: # found other token
+                if found_token.__class__.__name__.lower() == token_type_of_target:
+                    method_caller.call_method(method, [found_token])
+
+    def _handle_token_not_sensing_token_methods(self):
+        for method in self.board.event_handler.registered_events["on_not_sensing_token"].copy():
+            token = method.__self__
+            token_type_of_target = method.__name__[15:]
+            found_tokens_for_token_type = token.sensing_tokens(token_filter=token_type_of_target)
             if found_tokens_for_token_type == None:
                 found_tokens_for_token_type = []
+                method_caller.call_method(method, None)
+                return
             if token in found_tokens_for_token_type:
                 found_tokens_for_token_type.remove(token)
             for found_token in found_tokens_for_token_type:
                 if found_token.__class__.__name__.lower() == token_type_of_target:
-                    method_caller.call_method(method, [found_token])
+                    return
+            method_caller.call_method(method, None)
 
-    def _handle_collision_with_borders(self):
+
+    def _handle_token_sensing_border_methods(self):
         for event in self.board.event_handler.border_events:
             for method in self.board.event_handler.registered_events[event]:
                 sensed_borders = method.__self__.sensing_borders()
@@ -32,20 +50,20 @@ class BoardCollisionHandler:
                     method_caller.call_method(
                         method, [sensed_borders])
                 else:
-                    self._handle_specific_border(method, sensed_borders)
+                    self._handle_token_sensing_specific_border_methods(method, sensed_borders)
 
-    def _handle_specific_border(self, method, sensed_borders):
+    def _handle_token_sensing_specific_border_methods(self, method, sensed_borders):
         for border in sensed_borders:
             if border in method.__name__:
                 method_caller.call_method(method, None)
 
-    def _handle_on_board(self):
+    def _handle_token_sensing_on_board_methods(self):
         for method in self.board.event_handler.registered_events["on_sensing_on_board"].copy():
             is_on_board = method.__self__.sensing_on_board()
             if is_on_board:
                 method_caller.call_method(method, None)
 
-    def _handle_not_on_board(self):
+    def _handle_token_sensing_not_on_board_methods(self):
         for method in self.board.event_handler.registered_events["on_sensing_not_on_board"].copy():
             is_not_on_board = not method.__self__.sensing_on_board()
             if is_not_on_board:
