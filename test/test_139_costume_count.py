78ea96fba8e7297c02c30b83b31dd2bc7c84c44f
Andreas Siebel <andreas.siebel@it-teaching.de>
2024-03-12 09:39:41 +0100

    refactored test 138-147

--- test/test_139_costume_count.py
+++ test/test_139_costume_count.py
@@ -1,78 +1,42 @@
-from miniworldmaker import *
-import imgcompare
-import os
+from miniworldmaker import App, Board, Token, Position, CostumeOutOfBoundsError
+from .screenshot_tester import ScreenshotTester
 import unittest
+import os
+import random
 
-def diff(ia, ib):
-    percentage = imgcompare.image_diff_percent(ia, ib)
-    return percentage
 
 class Test139(unittest.TestCase):
-    
+
     def setUp(self):
-        """ Programmcode here:
-            You need:
-            
-            a) Your board must have the name `board`
-            b) init_test() must be called in on_setup,
-            or additionally on_setup-Method must be added
-            b) An act-method, where self.test() is called
-        """
-        App.reset(unittest = True, file = __file__)
-        
-        board = Board()
-        token = Token()
-        assert token.costume_count == 0
-        token.add_costume((255,0,0,0))
-        assert token.costume_count == 1
+        def test_code():
+            board = Board()
+            # Here comes your code
+            @board.register
+            def setup_environment(self, test):
+                token = Token()
+                assert token.costume_count == 0
+                token.add_costume((255,0,0,0))
+                assert token.costume_count == 1
+            return board
+        App.reset(unittest=True, file=__file__)
+        board = test_code()
+        """ Setup screenshot tester"""
+        TEST_FRAMES = [1]
+        QUIT_FRAME = 1
+        tester = ScreenshotTester(TEST_FRAMES, QUIT_FRAME, self)
+        tester.setup(board)
+        if hasattr(board, "setup_environment"):
+            board.setup_environment(self)
 
 
-        """ here act and init - delete if used in testcode"""
-        
-        @board.register
-        def on_setup(self):
-            self.init_test()
-            
-        @board.register
-        def act(self):
-            self.test()
-        
-        """ end of setUp - code up here""" 
-        
-        self.board = board
-           
-        @board.register
-        def init_test(self):
-            print("setup test")
-            board.test_frame = 0
-            
-        @board.register
-        def test(self):
-            print("test")
-            self.test_frame = self.test_frame + 1
-            if self.test_frame == 1:
-                print("Screenshot")
-                path = os.path.dirname(__file__)
-                if path != "":
-                    path =  path + "/"
-                file_test = path + f'output/{self.test_title}_test.png'
-                file_output = path + f"output/{self.test_title}.png"
-                if not os.path.isfile(file_test):
-                    board.screenshot(file_test)
-                board.screenshot(file_output)
-                d = diff(file_test, file_output)
-                assert 0 <= d <= 0.05
-                self.quit()
-        
-        #in TESTXYZ setup:
-        board.test_title = self.__class__.__name__
-        
-        
-    def test_108(self):
+
+        return board
+
+    def test_main(self):
         with self.assertRaises(SystemExit):
             self.board.run()
-        
-if __name__ == '__main__':
-    unittest.main()
 
+
+if __name__ == "__main__":
+    unittest.main()
 
--- test/test_147_colored_hexboard.py
+++ test/test_147_colored_hexboard.py
@@ -1,87 +1,45 @@
-from miniworldmaker import *
-import imgcompare
-import os
+from miniworldmaker import App, Board, Token, Position, HexBoard
+from .screenshot_tester import ScreenshotTester
 import unittest
+import os
 import random
 
-TEST_FRAMES = [1]
-QUIT_FRAME = 1
-    
-def diff(ia, ib):
-    percentage = imgcompare.image_diff_percent(ia, ib)
-    return percentage
 
 class Test147(unittest.TestCase):
 
     def setUp(self):
-        """ Programmcode here:
-            You need:
-            
-            a) Your board must have the name `board`
-            b) init_test() must be called in on_setup,
-            or additionally on_setup-Method must be added
-            b) An act-method, where self.test() is called
-        """
-        App.reset(unittest = True, file = __file__)
-        
-        board = HexBoard(24, 2)
+        def test_code():
+            board = HexBoard(24, 2)
+            # Here comes your code
+            @board.register
+            def setup_environment(self, test):
+                board.tile_size = 40
+                assert board.columns == 24
+                assert board.rows == 2
+                for x in range(board.columns):
+                    for y in range(board.rows):
+                        t = Token((x,y))
+                        color = (x + y) / (board.columns + board.rows) * 255
+                        t.fill_color = (color, color, color)
+            return board
+        App.reset(unittest=True, file=__file__)
+        board = test_code()
+        """ Setup screenshot tester"""
+        TEST_FRAMES = [1]
+        QUIT_FRAME = 1
+        tester = ScreenshotTester(TEST_FRAMES, QUIT_FRAME, self)
+        tester.setup(board)
+        if hasattr(board, "setup_environment"):
+            board.setup_environment(self)
 
-        board.tile_size = 40
-        assert board.columns == 24
-        assert board.rows == 2
-        for x in range(board.columns):
-            for y in range(board.rows):
-                t = Token((x,y))
-                color = (x + y) / (board.columns + board.rows) * 255
-                t.fill_color = (color, color, color)
-        """ here act and init - delete if used in testcode"""
-        
-        @board.register
-        def on_setup(self):
-            self.init_test()            
-            
-        @board.register
-        def act(self):
-            self.test()
-            
-        """ end of setUp - code up here""" 
-        
-        self.board = board
-           
-        @board.register
-        def init_test(self):
-            board.test_frame = 0
-        
-        @board.register
-        def test(self):
-            global TEST_FRAMES
-            global QUIT_FRAME
-            
-            self.test_frame = self.test_frame + 1
-            if self.test_frame in TEST_FRAMES:
-                print("screenshot test at frame",  self.test_frame)
-                path = os.path.dirname(__file__)
-                if path != "":
-                    path =  path + "/"
-                file_test = path + f'output/{self.test_title}_test_{self.test_frame}.png'
-                file_output = path + f"output/{self.test_title}_{self.test_frame}.png"
-                if not os.path.isfile(file_test):
-                    board.screenshot(file_test)
-                board.screenshot(file_output)
-                d = diff(file_test, file_output)
-                assert 0 <= d <= 0.05
-            if self.test_frame == QUIT_FRAME:
-                self.quit()
-        
-        #in TESTXYZ setup:
-        board.test_title = self.__class__.__name__
-        
-        
+
+
+        return board
+
     def test_main(self):
         with self.assertRaises(SystemExit):
             self.board.run()
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
-
-
