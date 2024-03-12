9aaac82bbbeda137702025682f071fbe5dd41649
Andreas Siebel <andreas.siebel@it-teaching.de>
2024-03-12 13:32:30 +0100

    refactored test 214-219

--- test/test_216_pixel_board_move_to_position.py
+++ test/test_216_pixel_board_move_to_position.py
@@ -1,90 +1,47 @@
-from miniworldmaker import *
-import imgcompare
-import os
+from miniworldmaker import App, Board, Token, Position, PixelBoard
+from .screenshot_tester import ScreenshotTester
 import unittest
+import os
 import random
 
-TEST_FRAMES = [1, 10, 30, 40, 50, 60, 90, 120, 150, 180, 210, 240, 270]
-QUIT_FRAME = 270
-    
-def diff(ia, ib):
-    percentage = imgcompare.image_diff_percent(ia, ib)
-    return percentage
 
 class Test216(unittest.TestCase):
 
     def setUp(self):
-        """ Programmcode here:
-            You need:
-            
-            a) Your board must have the name `board`
-            b) init_test() must be called in on_setup,
-            or additionally on_setup-Method must be added
-            b) An act-method, where self.test() is called
-        """
-        App.reset(unittest = True, file = __file__)
+        def test_code():
+            board = PixelBoard()
+            # Here comes your code
+            @board.register
+            def setup_environment(self, test):
+                board.columns = 400
+                board.rows = 400
+                player = Token()
+                player.add_costume("images/player_1.png")
+                player.position = (200,200)
+
+                @player.register
+                def act(self):
+                    self.move_towards((280, 260))
+            return board
+        App.reset(unittest=True, file=__file__)
+        board = test_code()
+        """ Setup screenshot tester"""
+        TEST_FRAMES = [1, 10, 30, 40, 80]
+        QUIT_FRAME = 81
+    
+        tester = ScreenshotTester(TEST_FRAMES, QUIT_FRAME, self)
+        tester.setup(board)
+        if hasattr(board, "setup_environment"):
+            board.setup_environment(self)
 
 
-        board = PixelBoard()
-        board.columns = 400
-        board.rows = 400
-        player = Token()
-        player.add_costume("images/player_1.png")
-        player.position = (200,200)
 
-        @player.register
-        def act(self):
-            self.move_towards((360, 360))
+        return board
 
-        """ here act and init - delete if used in testcode"""
-        
-        @board.register
-        def on_setup(self):
-            self.init_test()            
-            
-        @board.register
-        def act(self):
-            self.test()
-            
-        """ end of setUp - code up here""" 
-        
-        self.board = board
-           
-        @board.register
-        def init_test(self):
-            print("setup test")
-            board.test_frame = 0
-        
-        @board.register
-        def test(self):
-            global TEST_FRAMES
-            global QUIT_FRAME
-            
-            self.test_frame = self.test_frame + 1
-            if self.test_frame in TEST_FRAMES:
-                print("screenshot test at frame",  self.test_frame)
-                path = os.path.dirname(__file__)
-                if path != "":
-                    path =  path + "/"
-                file_test = path + f'output/{self.test_title}_test_{self.test_frame}.png'
-                file_output = path + f"output/{self.test_title}_{self.test_frame}.png"
-                if not os.path.isfile(file_test):
-                    board.screenshot(file_test)
-                board.screenshot(file_output)
-                d = diff(file_test, file_output)
-                assert 0 <= d <= 0.05
-            if self.test_frame == QUIT_FRAME:
-                self.quit()
-        
-        #in TESTXYZ setup:
-        board.test_title = self.__class__.__name__
-        
-        
     def test_main(self):
         with self.assertRaises(SystemExit):
             self.board.run()
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
-
-
