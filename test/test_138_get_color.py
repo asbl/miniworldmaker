92dda588214a6f40d94169365bdf369e984f2d3e
Andreas Siebel <andreas.siebel@it-teaching.de>
2024-03-12 07:52:14 +0100

    refactored test 133-138

--- test/test_138_get_color.py
+++ test/test_138_get_color.py
@@ -6,66 +6,67 @@ import random
 
 TEST_FRAMES = [1]
 QUIT_FRAME = 20
-    
+
+
 def diff(ia, ib):
     percentage = imgcompare.image_diff_percent(ia, ib)
     return percentage
 
+
 class Test138(unittest.TestCase):
 
     def setUp(self):
-        """ Programmcode here:
-            You need:
-            
-            a) Your board must have the name `board`
-            b) init_test() must be called in on_setup,
-            or additionally on_setup-Method must be added
-            b) An act-method, where self.test() is called
-        """
-        App.reset(unittest = True, file = __file__)
-        
+        """Programmcode here:
+        You need:
 
-        board = Board((100,60))
+        a) Your board must have the name `board`
+        b) init_test() must be called in on_setup,
+        or additionally on_setup-Method must be added
+        b) An act-method, where self.test() is called
+        """
+        App.reset(unittest=True, file=__file__)
+
+        board = Board((100, 60))
 
         @board.register
         def on_setup(self):
-            self.add_background((255,0,0))
-            assert(self.get_color_from_pixel((5,5)) == (255, 0, 0, 255))
+            self.add_background((255, 0, 0))
+            assert self.get_color_from_pixel((5, 5)) == (255, 0, 0, 255)
             self.init_test()
 
-
-
         """ here act and init - delete if used in testcode"""
-        
-        #@board.register
-        #def on_setup(self):
+
+        # @board.register
+        # def on_setup(self):
         #    self.init_test()
-            
+
         @board.register
         def act(self):
             self.test()
-            
-        """ end of setUp - code up here""" 
-        
+
+        """ end of setUp - code up here"""
+
         self.board = board
-           
+
         @board.register
         def init_test(self):
             print("setup test")
             board.test_frame = 0
-        
+
         @board.register
         def test(self):
             global TEST_FRAMES
             global QUIT_FRAME
-            
+
             self.test_frame = self.test_frame + 1
             if self.test_frame in TEST_FRAMES:
-                print("screenshot test at frame",  self.test_frame)
+                print("screenshot test at frame", self.test_frame)
                 path = os.path.dirname(__file__)
                 if path != "":
-                    path =  path + "/"
-                file_test = path + f'output/{self.test_title}_test_{self.test_frame}.png'
+                    path = path + "/"
+                file_test = (
+                    path + f"output/{self.test_title}_test_{self.test_frame}.png"
+                )
                 file_output = path + f"output/{self.test_title}_{self.test_frame}.png"
                 if not os.path.isfile(file_test):
                     board.screenshot(file_test)
@@ -74,16 +75,14 @@ class Test138(unittest.TestCase):
                 assert 0 <= d <= 0.05
             if self.test_frame == QUIT_FRAME:
                 self.quit()
-        
-        #in TESTXYZ setup:
+
+        # in TESTXYZ setup:
         board.test_title = self.__class__.__name__
-        
-        
+
     def test_main(self):
         with self.assertRaises(SystemExit):
             self.board.run()
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
-
-
